8e17db10a74c003643cc75b0e712724a60572265
Vững Phạm Văn <vung0222166@huce.edu.vn>
2025-01-18 09:32:01 +0700

    initial commit

---
+++ src/test/java/org/example/authenticationwiththymeleaf/AuthenticationWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authenticationwiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
